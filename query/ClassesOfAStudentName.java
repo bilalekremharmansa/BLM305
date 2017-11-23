c471fc2239b3a3596be54f8dceddffd322c8020c
Salim Şahin <salim.sahin@stu.fsm.edu.tr>
2017-11-23 18:23:58 +0300

    ClassesOfAStudentName - Salim Şahin

---
+++ query/ClassesOfAStudentName.java
@@ -0,0 +1,27 @@
+package query;
+import java.util.*;
+
+public class ClassesOfAStudentName implements Query {
+    public String defaultValue() { return "mehmet"; }
+    public String question() {
+        return "List of classes with given student name";
+    }
+    public String doQuery(Database db, String name) {
+        Set<String>courses = new TreeSet<>();
+        if (name == null) name = defaultValue();
+        String result = name+"\n";
+        for (Student s : db.std.values())
+            if (s.name.equals(name.toUpperCase())) {
+                for (String crs: s.courses){
+                    courses.addAll(Arrays.asList(db.findCourse(crs).rooms));
+                }
+            }
+        for (String s: courses){
+            result += (s + " ");
+        }
+        courses = null;
+        System.out.println(courses.size()+" classes: "+name);
+        return result;
+    }
+    public String author() { return "Salim Şahin"; }
+}
